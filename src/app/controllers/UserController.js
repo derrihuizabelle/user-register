91eb5273484f8e0daa92fe8b9a673a87af963491
Izabelle Araujo <izabellederrihu64@gmail.com>
2020-11-21 20:14:32 -0300

    feat: index, show, delete and update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,7 +2,60 @@ import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
-  // Only a single register
+  async show(req, res) {
+    try {
+      const schema = Yup.object().shape({
+        id: Yup.string().required(),
+      });
+
+      if (!(await schema.isValid(req.query))) {
+        return res.json({ error: 'Validation fail' });
+      }
+
+      const userExists = await User.findOne({
+        where: {
+          id: req.query.id,
+        },
+      });
+
+      if (!userExists) {
+        return res.status(400).json({
+          error: 'User dont exist',
+        });
+      }
+
+      return res.json({
+        userExists,
+      });
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const schema = Yup.object().shape({});
+
+      if (!(await schema.isValid(req.body))) {
+        return res.json({ error: 'Validation fail' });
+      }
+
+      const userList = await User.findAll();
+
+      if (!userList) {
+        return res.status(400).json({
+          error: 'Empty list',
+        });
+      }
+
+      return res.json({
+        userList,
+      });
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  }
+
   async store(req, res) {
     try {
       const schema = Yup.object().shape({
@@ -42,9 +95,71 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.status(200).json({
-      msg: 'To starfleet command, were authorized!',
-    });
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().email().required(),
+        password: Yup.string().required().min(6),
+      });
+
+      if (!(await schema.isValid(req.body))) {
+        return res.json({
+          error: 'Validation faill',
+        });
+      }
+
+      const userExists = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      if (!userExists) {
+        return res.status(400).json({
+          error: 'User dont exist',
+        });
+      }
+
+      const response = await User.update(req.body, {
+        where: { id: userExists.id },
+      });
+
+      return res.json({
+        response,
+      });
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const schema = Yup.object().shape({
+        id: Yup.string().required(),
+      });
+
+      if (!(await schema.isValid(req.query))) {
+        return res.json({ error: 'Validation fail' });
+      }
+
+      const deleted = await User.destroy({
+        where: {
+          id: req.query.id,
+        },
+      });
+
+      if (!deleted) {
+        return res.status(400).json({
+          error: 'User cant be deleted',
+        });
+      }
+
+      return res.json({
+        deleted,
+      });
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,22 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import AuthMiddleware from './app/middlewares/auth';
 
+// put the token on Bearer -> token
+
 const router = new Router();
 
-router.get('/', (req, res) => {
+router.get('/test', (req, res) => {
   return res
     .status(200)
     .json({ message: 'Uss Enterprise to starfleet command, were online!' });
+});
+
+router.get('/user-info', AuthMiddleware, (req, res) => {
+  return UserController.show(req, res);
+});
+
+router.get('/user-list', AuthMiddleware, (req, res) => {
+  return UserController.index(req, res);
 });
 
 router.post('/new-user', (req, res) => {
@@ -17,6 +27,12 @@ router.post('/new-user', (req, res) => {
 
 router.post('/login', SessionController.store);
 
-router.put('/update-user', AuthMiddleware, UserController.update);
+router.put('/update-user', AuthMiddleware, (req, res) => {
+  return UserController.update(req, res);
+});
+
+router.delete('/delete-user', AuthMiddleware, (req, res) => {
+  return UserController.delete(req, res);
+});
 
 export default router;
